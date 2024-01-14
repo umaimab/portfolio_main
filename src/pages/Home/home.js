fb30a50dc24e687fdc1990aa04e6779943c64521
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2024-01-14 19:15:27 +0300

    Added 1-3 homepage panels

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,57 @@
+import React, { useEffect } from 'react';
 import Navbar from '../../components/navbar/navbar'
+import './home.css';
+import '../../styles/global.css';
 
 const Home = () => {
+  useEffect(() => {
+    const container = document.getElementById('secondImgContainer');
+    
+    const handleWheelScroll = (event) => {
+      event.preventDefault();
+      container.scrollLeft += event.deltaY;
+    };
+
+    container.addEventListener('wheel', handleWheelScroll);
+
+    return () => {
+      container.removeEventListener('wheel', handleWheelScroll);
+    };
+  }, []);
   
     return (
-      <Navbar />
+      <div className='firstContainer'>
+        <Navbar />
+        <div className='firstPanel'>
+          <div className='firstImg'>First Image</div>
+          <div className='firstHeading'>First Heading</div>
+        </div>
+
+        <div className='secondPanel'>
+          <div className='secondHeading'>Second Heading</div>
+          <div className='secondImg' id='secondImgContainer'>
+            <div className='imageRow'>Image 1</div>
+            <div className='imageRow'>Image 2</div>
+            <div className='imageRow'>Image 3</div>
+            <div className='imageRow'>Image 4</div>
+            <div className='imageRow'>Image 5</div>
+          </div>
+        </div>
+
+        <div className='thirdPanel'>
+          <div className='thirdHeading'>Third Heading</div>
+          <div className='thirdText' >
+            <ul className='designList'>
+              <li>Bullet 1</li>
+              <li>Bullet 2</li>
+              <li>Bullet 3</li>
+              <li>Bullet 4</li>
+              <li>Bullet 5</li>
+              <li>Bullet 6</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     )
   }
 
