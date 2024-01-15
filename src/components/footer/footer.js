b6ade26847834a8f7843d9408ff5fcb65be8ef1a
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2024-01-15 15:25:20 +0300

    Removed placeholder bg colors and added titles and content text

---
+++ src/components/footer/footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  
+    return (
+        <div className='footer'>
+            <div>
+                <h3>UMAIMA<br />BAJAMAN</h3>
+                <p>Graphic Designer | EXPRESSING CREATIVITY IN PIXELS AND PRINT</p>
+            </div>
+            <div>
+                <h3>CONTACT</h3> 
+                <p>Email: umaima.bajaman@gmail.com</p>
+                <p>LinkedIn: www.linkedin.com/in/umaimab</p>
+            </div>
+        </div>
+    )
+  }
+
+
+export default Footer
