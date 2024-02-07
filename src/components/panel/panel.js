0a39e3c481e3ab467c911cb4b63e8ac6a6db9bd0
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2024-02-07 06:55:53 +0300

    Added transition to panel heading

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Panel.css';
 import Text from '../text/Text';
+import Heading from '../heading/Heading';
 
 const Panel = ({image, heading, subheading, tagline, description}) => {
     return (
       <div className='section'>
         <img src={image} alt={heading} className='section__image-padding'/>
           <div className='panel'>
-            <h1 className='panel__heading'>{heading}</h1>
+            <Heading>{heading}</Heading>
             <div>
               <p className='panel__subheading'>{subheading}</p>
               <p className='panel__tag'>{tagline}</p>
